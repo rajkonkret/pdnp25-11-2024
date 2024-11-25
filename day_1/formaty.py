4924d85df82bcbfd0a55badd5bfae182a02416b2
raajkonkret <rajkonkret660@gmail.com>
2024-11-25 14:12:46 +0100

    [1.8] add formaty.py

---
+++ day_1/formaty.py
@@ -0,0 +1,7 @@
+user = "Tomek"  # str
+wiek = 39  # int
+wersja = 3.90001  # <class 'float'>
+print(type(wersja))  # <class 'float'>, liczby zmiennoprzecinkowe
+liczba = 5678907654321  # int
+
+print("Witaj %s, masz teraz %d lat" % (user, wiek))
