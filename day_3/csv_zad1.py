b89182e95e467d972348c670cb46282ba7bdad83
rajkonkret <rajkonkret660@gmail.com>
2024-12-13 16:19:40 +0100

    add pliki_zad1.py

---
+++ day_3/csv_zad1.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+# pip install pandas
+
+data = pd.read_csv('dane.csv')
+
+print(data)
+#               Kowalski       Jan              Kłodzko
+# 0                Nowak     Zenon             Szczecin
+# 1  Brzęczyszczykiewicz  Grzegorz  Chrząszczyżewoszyce
